d3baef7824f86c5ec92423fd9d3bb854e3004229
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-30 02:27:11 +0000

    automatically backed up by learn

--- crewMember.js
+++ crewMember.js
@@ -2,4 +2,12 @@ class CrewMember {
   constructor(position) {
     this.position = position;
   }
+
+  currentShip() {
+    if(this.ship) {
+      return this.ship;
+    } else {
+      return "Looking for a Rig";
+    }
+  }
 }
