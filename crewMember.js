cf85ca5b01b709513121fd31e5eb53f3ef80bb8b
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-30 02:46:22 +0000

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,31 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = "Looking for a Rig";
+  }
+
+  engageWarpDrive() {
+    if (this.position === "Pilot" && this.currentShip !== "Looking for a Rig"){
+      return;
+    } else {
+      return "had no effect";
+    }
+  }
+
+  setsInvisibility() {
+    if (this.position === "Defender" && this.currentShip !== "Looking for a Rig" ){
+      this.currentShip.cloaked = true;
+    } else {
+      return "had no effect";
+    }
+  }
+
+  chargePhasers() {
+    if (this.position === "Gunner" && this.currentShip !== "Looking for a Rig"){
+      return;
+    } else {
+      return "had no effect";
+    }
+  }
+
+}
--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,19 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = 'disengaged';
+    this.phasersCharge = 'uncharged';
+    if(this.crew.length === 0) {
+      this.docked = true;
+    } else {
+      this.docked = false;
+      this.crew.forEach( (e) => {
+        e.currentShip = this;
+      });
+    }
+  }
+}
